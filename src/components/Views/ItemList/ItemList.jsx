96952243809bd5f1fa76186a33a1391a366fc2eb
Quan L. <45319129+Snowflaker1080@users.noreply.github.com>
2025-10-25 09:01:59 +1100

    paginiation update

--- src/components/Views/ItemList/ItemList.jsx
+++ src/components/Views/ItemList/ItemList.jsx
@@ -320,12 +320,12 @@ const ItemList = ({
                         }}
                         style={{ marginLeft: 8 }}
                       >
-                        <option value={5}>5</option>
                         <option value={10}>10</option>
                         <option value={20}>20</option>
-                        <option value={30}>30</option>
                         <option value={40}>40</option>
                         <option value={50}>50</option>
+                        <option value={50}>50</option>
+                        <option value={100}>100</option>
                       </select>
                       per page
                     </label>
